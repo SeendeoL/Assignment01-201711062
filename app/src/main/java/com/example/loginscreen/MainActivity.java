899229b2b43f8bc938b8bb9572e4df1885ede9fc
SeendeoL <oguzhantarhanankara@gmail.com>
2022-03-17 22:43:30 +0300

    Seng-405 upgraded.

--- app/src/main/java/com/example/loginscreen/MainActivity.java
+++ app/src/main/java/com/example/loginscreen/MainActivity.java
@@ -3,12 +3,47 @@ package com.example.loginscreen;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText username;
+    EditText password;
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        button = findViewById(R.id.login_button);
+
+        Button login_button = (Button) findViewById(R.id.login_button);
+
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                velidateEmail(username);
+            }
+        });
+
+
+    }
+    private boolean velidateEmail(EditText username){
+     String emailInput = username.getText().toString();
+
+     if(!emailInput.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(emailInput).matches()){
+         Toast.makeText(this, "Email Validated Successfully", Toast.LENGTH_SHORT).show();
+         return true;
+     }
+     else{
+         Toast.makeText(this, "Invalid Email Address", Toast.LENGTH_SHORT).show();
+         return false;
+     }
     }
 }
